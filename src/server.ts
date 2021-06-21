c80473f309ff8fb952dd8c6ae92792bde226b2d2
Alex Oliveira <alex_oliveira08@yahoo.com.br>
2021-06-21 20:11:57 -0300

    Create GET and POST method

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,16 @@ const app = express();
  *  PATH    =>  Alterar uma informação específica
  */
 
+app.get("/test", (request, response) => {
+    // Request => Entrando
+    // Response => Saindo
+
+    return response.send("Olá NLW");
+});
+
+app.post("/test-post", (request, response) => {
+    return response.send("Olá NLW método POST");
+});
 
 // http://localhost:3000
 app.listen(3000, () => console.log("Server is running"));
