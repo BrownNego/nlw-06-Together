fad9ac830302557a4e394b8a76d20a9c53b1bf35
Alex Oliveira <alex_oliveira08@yahoo.com.br>
2021-06-22 22:26:04 -0300

    Create database

---
+++ src/database/migrations/1624411313346-CreateUsers.ts
@@ -0,0 +1,12 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateUsers1624411313346 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
