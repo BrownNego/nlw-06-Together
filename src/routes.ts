c324c85c3b07ec15242a6bebd93b49d9b59f7d6a
Alex Oliveira <alex_oliveira08@yahoo.com.br>
2021-06-24 21:59:09 -0300

    handling exceptions

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { CreateuserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
 
 const createUserController = new CreateuserController();
 const createTagController = new CreateTagController();
 
-router.post("/users", createUserController.handle)
-router.post("/tags", createTagController.handle)
+router.post("/users", createUserController.handle);
+router.post("/tags", ensureAdmin, createTagController.handle);
 
 
 
