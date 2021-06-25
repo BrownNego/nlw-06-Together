3d021635a5b8744057e4a91e0689805c97160532
Alex Oliveira <alex_oliveira08@yahoo.com.br>
2021-06-24 21:37:17 -0300

    create tag service

--- src/services/CreateTagService.ts
+++ src/services/CreateTagService.ts
@@ -1,9 +1,35 @@
+import { getCustomRepository } from "typeorm"
+import { TagsRepositories } from "../repositories/TagsRepositories"
+
+
 
 
 
 class CreateTagService {
 
-    async execute() {
+    async execute(name: string) {
+        const tagsRepositories = getCustomRepository(TagsRepositories);
+
+        if (!name) {
+            throw new Error("Incorrect name!");
+        }
+
+        // SELECT * FROM TAGS WHERE NAME = 'name'
+        const tagAlredyExists = await tagsRepositories.findOne({
+            name,
+        });
+
+        if (tagAlredyExists) {
+            throw new Error("Tag already exists!");
+        }
+
+        const tag = tagsRepositories.create({
+            name,
+        });
+
+        await tagsRepositories.save(tag);
+
+        return tag;
 
     }
 
