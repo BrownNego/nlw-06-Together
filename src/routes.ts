15ce4227f68827093df7eff3d8e56efc3d06af4c
Alex Oliveira <alex_oliveira08@yahoo.com.br>
2021-06-24 21:47:30 -0300

    create tag router

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { CreateuserController } from "./controllers/CreateUserController";
+import { CreateTagController } from "./controllers/CreateTagController";
 
 const router = Router();
 
 const createUserController = new CreateuserController();
+const createTagController = new CreateTagController();
 
 router.post("/users", createUserController.handle)
+router.post("/tags", createTagController.handle)
 
 
 
-export { router }
+export { router };
